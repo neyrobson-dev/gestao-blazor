5d9ef9590809484b9b4f5c74220fc880821a1425
Ney Robson Araujo Gama <ngama@coamo.com.br>
2025-04-13 13:21:31 -0300

    WIP

---
+++ Gestão.Domain/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Gestão.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+    }
+}
---
+++ Gestão.Domain/Company.cs
@@ -0,0 +1,21 @@
+﻿using Gestao.Data;
+
+namespace Gestão.Domain
+{
+    internal class Company
+    {
+        public int Id { get; set; }
+        public string LegalName { get; set; } = string.Empty;
+        public string TradeName { get; set; } = string.Empty;
+        public string TaxId { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Neighborhood { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Complement { get; set; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; set; }
+        public Guid UserId { get; set; }
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
